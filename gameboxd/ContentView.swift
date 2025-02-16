7bb04f44cc536c9baa6ac6e58fa8483d3d763b46
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2025-02-15 22:24:38 -0500

    Initial Commit

---
+++ gameboxd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gameboxd
+//
+//  Created by Arshdeep Singh on 2/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gameboxd/gameboxdApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameboxdApp.swift
+//  gameboxd
+//
+//  Created by Arshdeep Singh on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct gameboxdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
